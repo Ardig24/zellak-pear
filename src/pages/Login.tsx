aca285185b8f995f2c619d94b109717623c2eb5d
Ardig24 <150798620+Ardig24@users.noreply.github.com>
2025-03-21 15:25:04 +0100

    Update Login.tsx with logo for mobile version

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -32,7 +32,7 @@ export default function Login() {
       </div>
 
       <div className="card w-full max-w-[90%] sm:max-w-md bg-white/30 backdrop-blur-md shadow-2xl border border-white/50 px-6 pt-32 pb-10 sm:px-8 sm:pt-40 sm:pb-14 relative">
-        <div className="absolute top-0 left-0 right-0 flex justify-center" style={{transform: 'translateY(-20%)'}}>
+        <div className="absolute top-0 left-0 right-0 flex justify-center z-10" style={{transform: 'translateY(-20%)'}}>
           <img src="/logo-login.png?v=1" alt="Zellak Logo" className="h-256 w-auto" />
         </div>
         <div className="text-center mb-6 sm:mb-8">
